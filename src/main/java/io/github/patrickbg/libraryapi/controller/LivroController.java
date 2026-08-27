ed0e7ab79e664df6b9f31682d6a87ab2f0e2f1fe
Patrick Borges <patrickpborges1996@gmail.com>
2026-08-27 15:55:13 -0300

    MapStruct configuration

--- src/main/java/io/github/patrickbg/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/patrickbg/libraryapi/controller/LivroController.java
@@ -1,6 +1,15 @@
 package io.github.patrickbg.libraryapi.controller;
 
+import io.github.patrickbg.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.patrickbg.libraryapi.controller.dto.ErroResposta;
+import io.github.patrickbg.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.patrickbg.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("livros")
 @RequiredArgsConstructor
 public class LivroController {
+
+    private final LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto){
+        try{
+            //
+            return ResponseEntity.ok(dto);
+
+        }catch (RegistroDuplicadoException e){
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(erroDTO);
+        }
+    }
 }
--- src/main/java/io/github/patrickbg/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/patrickbg/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,15 +1,26 @@
 package io.github.patrickbg.libraryapi.controller.dto;
 
 import io.github.patrickbg.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record CadastroLivroDTO(String isbn,
-                               String titulo,
-                               LocalDate dataPublicacao,
-                               GeneroLivro genero,
-                               BigDecimal preco,
-                               UUID idAutor) {
+public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatorio")
+        String isbn,
+        @NotBlank(message = "campo obrigatorio")
+        String titulo,
+        @NotNull(message = "campo obrigatorio")
+        @Past(message = "não pode ser uma data futura")
+        LocalDate dataPublicacao,
+        GeneroLivro genero,
+        BigDecimal preco,
+        @NotNull(message = "campo obrigatorio")
+        UUID idAutor) {
 }
